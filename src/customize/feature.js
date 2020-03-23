729d0fcf461141551ab96f29c3153e0a5e0e0476
Chris Aragon <aragonchristopherray@gmail.com>
2020-03-23 16:37:11 -0400

    fixed the typo

--- src/customize/feature.js
+++ src/customize/feature.js
@@ -5,7 +5,7 @@ export default class Feature extends Component {
     render () {
         //ok ok ok wtf goes in the .keys() method; its not happy when its features
         //console.log(this.props.selected) is defined as the STORE from App.js
-        const features = Object.key(this.props.selected).map((feature, idx) => {
+        const features = Object.keys(this.props.selected).map((feature, idx) => {
         const featureHash = feature + '-' + idx;
         return (
         <fieldset className="feature" key={featureHash}>
